cfbff496b7171154cfadd806d5b5ee971d5d437c
audreysharp <audreysharp21@gmail.com>
2016-11-12 19:41:51 -0500

    Finalize category links

--- README.md
+++ README.md
@@ -2,53 +2,24 @@
 
 Project for YHack 2016
 
-<!-- ON CLICK
--Have 8 sequential trending video requests (5 per page, total 40)
--Store each video object info in a list
--Pick a random video from the list and link to that video
- -->
+Google Chrome extension for YouTube video discovery
 
-ON CLICK
--Make request to get topic page source code through Yahoo API
--Parse through videos from JSON file and put them in a list
--Pick a random video from the list and link to that video
+## Extension Function
+- Make request to get topic page source code through [Yahoo YQL](https://developer.yahoo.com/yql/) API
+- Parse through videos from JSON file and put them in a list
+- Pick a random video from the list and link to that video
 
-TOPICS
+## Categories
 Music
-https://www.youtube.com/playlist?list=PLFgquLnL59alCl_2TQvOiD5Vgm1hCaGSI
-		
 Sports
-https://www.youtube.com/playlist?list=PL8fVUTBmJhHKEJjTNWn-ykf67rVrFWYtC
-				
 Gaming	
-https://www.youtube.com/playlist?list=PLiCvVJzBupKnKoAJR3T8NxXwA5mPeBD8W
-	
 Movies & TV
-https://www.youtube.com/channel/UCtTnz5C5E4WRUSyb_EVVQeg/videos
-		
 News	
-https://www.youtube.com/playlist?list=PL3ZQ5CpNulQldOL3T8g8k1mgWWysJfE9w
-	
 Education	
-https://www.youtube.com/playlist?list=PL2PswnKYChBjPFkEDGKDYsITPIzNfs1h0
-
 Politics
-https://www.youtube.com/playlist?list=PLUmcrN7u4BoyDfb9l6XNeAIO2Cs9k5GrM
-
 Food		
-Already in
-
 Art	
-https://www.youtube.com/playlist?list=PL54PBRyn-helE8EiEbqu0v5v6z7YGGwpC
-	
 Travel
-https://www.youtube.com/playlist?list=PLrebgFpgoVVa8bPu3vrzDyczjada_yyBz
-
 Comedy
-https://www.youtube.com/playlist?list=PLHkIZA41nhArb4g7pjyW0ehiACFI3PXdf
-
 Fashion
-https://www.youtube.com/playlist?list=PLe8KJ5gtpzr9wRAaQpLWlfc4Y1Xa7hKx7
-
-Technology
-https://www.youtube.com/playlist?list=PL57quI9usf_vDPXuhqIjyrPIjkw3C1oPe
+Technolog
--- popup.js
+++ popup.js
@@ -9,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
   document.getElementById("categories").addEventListener("click", function(e) {
     if (e.target && e.target.nodeName == "A") {
-      //console.log(e.target.id);
       goToVideo(e.target.id)
     }
   });
@@ -32,11 +31,17 @@ function goToVideo(category) {
 
 var categoryLinks = {
   general: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Ffeed%2Ftrending%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
-  music: "",
-  gaming: "",
-  sports: "",
-  movies: "",
+  music: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPLFgquLnL59alCl_2TQvOiD5Vgm1hCaGSI%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
+  gaming: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPLiCvVJzBupKnKoAJR3T8NxXwA5mPeBD8W%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
+  sports: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPL8fVUTBmJhHKEJjTNWn-ykf67rVrFWYtC%0A%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
+  moviesTV: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fchannel%2FUCtTnz5C5E4WRUSyb_EVVQeg%2Fvideos%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
+  technology: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPL57quI9usf_vDPXuhqIjyrPIjkw3C1oPe%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
   food: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPL48XRvWqT9sLHDOyn-TWNNV9O63hBePrY%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
-  art: "",
-  news: "",
+  art: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPL54PBRyn-helE8EiEbqu0v5v6z7YGGwpC%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
+  news: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPL3ZQ5CpNulQldOL3T8g8k1mgWWysJfE9w%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
+  fashion: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPLe8KJ5gtpzr9wRAaQpLWlfc4Y1Xa7hKx7%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
+  politics: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPLUmcrN7u4BoyDfb9l6XNeAIO2Cs9k5GrM%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
+  comedy: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPLHkIZA41nhArb4g7pjyW0ehiACFI3PXdf%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
+  travel: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPLrebgFpgoVVa8bPu3vrzDyczjada_yyBz%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
+  education: "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22https%3A%2F%2Fwww.youtube.com%2Fplaylist%3Flist%3DPL2PswnKYChBjPFkEDGKDYsITPIzNfs1h0%22%20and%20xpath%3D%22%2F%2Fa%5Bcontains(%40href%2C%20'watch%3Fv%3D')%20and%20contains(%40class%2C%20'yt-uix-tile-link')%5D%22&format=json&diagnostics=true&callback=",
 }
